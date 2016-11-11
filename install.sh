1215c63c88944c27c6ef6e41bff2886d34a619f0
Benjamin Gelb <beg5670@gmail.com>
2016-11-11 14:07:29 -0500

    make bin dir if it doesnt exist

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 install() {
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/HaliteChallenge/Halite/master/environment/install.sh)"
+    # make dir if it doesn't exist
+    mkdir -p bin
     mv halite bin/
 }
 
